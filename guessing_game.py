5be1e0828e0b92086a0ba5e2fee143fbb04ac55b
Josh Witherell <witherellj24@gmail.com>
2022-07-23 22:16:45 -0400

    Added infinite loop, congratulations message,  and endgame code

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,29 @@
 import random
 
+print("-------->   Hello! Welcome to the Number Guessing Game!   <--------")
 
 def start_game():
-    print("-------->   Hello! Welcome to the Number Guessing Game!   <--------")
+    user_attempts = 1
     rand_number = random.randrange(1,11)
     number = int(input("Please select a number from 1 to 10.  "))
-    if number > rand_number:
-        print(f"Sorry, the correct number is lower than your guess of {number}. Please try again.")
-        number = int(input("Please select a different number from 1 to 10.  "))
-    elif number < rand_number:
-        print(f"Sorry, the correct number is higher than your guess of {number}. Please try again.")
-        number = input(int("Please select a different number from 1 to 10.  "))
+    while True:
+        if number > rand_number:
+            print(f"Sorry, the correct number is lower than your guess of {number}. Please try again.")
+            user_attempts += 1
+            number = int(input("Please select a different number from 1 to 10.  "))
+            continue
+        elif number < rand_number:
+            print(f"Sorry, the correct number is higher than your guess of {number}. Please try again.")
+            user_attempts += 1
+            number = int(input("Please select a different number from 1 to 10.  "))
+            continue
+        elif number == rand_number:
+            print(f"Congratulations! {number} is the correct number!")
+            if user_attempts == 1:
+                print(f"It took you {user_attempts} try.")
+            else:
+                print(f"It took you {user_attempts} tries.")
+            break
 
 
     """Psuedo-code Hints

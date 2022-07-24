2087c970ab1be23ebbe4b16e4b23df7bda2f1343
Josh Witherell <witherellj24@gmail.com>
2022-07-23 21:38:14 -0400

    Added user input of number and response to incorrect answers

--- guessing_game.py
+++ guessing_game.py
@@ -3,17 +3,20 @@ import random
 
 def start_game():
     print("-------->   Hello! Welcome to the Number Guessing Game!   <--------")
-    rand_number = random.randrange(0,11)
-    
+    rand_number = random.randrange(1,11)
+    number = int(input("Please select a number from 1 to 10.  "))
+    if number > rand_number:
+        print(f"Sorry, the correct number is lower than your guess of {number}. Please try again.")
+        number = int(input("Please select a different number from 1 to 10.  "))
+    elif number < rand_number:
+        print(f"Sorry, the correct number is higher than your guess of {number}. Please try again.")
+        number = input(int("Please select a different number from 1 to 10.  "))
+
+
     """Psuedo-code Hints
 
     When the program starts, we want to:
     ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
 
     4. Once the guess is correct, stop looping, inform the user they "Got it"
          and show how many attempts it took them to get the correct number.

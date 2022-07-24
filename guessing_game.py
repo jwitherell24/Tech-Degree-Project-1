ea8fd2c0113ec0ea097ea3c486f46138f084f4ac
Josh Witherell <witherellj24@gmail.com>
2022-07-23 22:22:55 -0400

    Added message indicating end of game

--- guessing_game.py
+++ guessing_game.py
@@ -23,23 +23,7 @@ def start_game():
                 print(f"It took you {user_attempts} try.")
             else:
                 print(f"It took you {user_attempts} tries.")
+                print("Thank you for playing the number guessing game!")
             break
 
-
-    """Psuedo-code Hints
-
-    When the program starts, we want to:
-    ------------------------------------
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
 start_game()

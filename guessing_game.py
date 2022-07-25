64c2ed6d759a8856492b590cb4c13894bd23e2bf
Josh Witherell <witherellj24@gmail.com>
2022-07-25 09:39:40 -0400

    Fixed crashing that would occur after two initial invalid values, restructured the error handling

--- guessing_game.py
+++ guessing_game.py
@@ -5,30 +5,28 @@ print("-------->   Hello! Welcome to the Number Guessing Game!   <--------")
 def start_game():
     user_attempts = 1
     rand_number = random.randrange(1,11)
+    rand_range_low = 1
+    rand_range_high = 10
 
-    try:
-        number = int(input("Please select a number from 1 to 10.  "))
-
-    except ValueError:
-        print("Sorry, that is not a valid value. Please guess again.")
-
-    else:
-        while True:
-            if number > rand_number:
-                print(f"Sorry, the correct number is lower than your guess of {number}. Please try again.")
+    while True:
+        try:
+            number = int(input(f"Please select a number from {rand_range_low} to {rand_range_high}.  "))
+        except ValueError:
+            print("Sorry, that is not a valid value. Please guess again.")
+        else:
+            if number != rand_number:
                 user_attempts += 1
-                try:
-                    number = int(input("Please select a different number from 1 to 10.  "))
-                except ValueError:
-                    print("Sorry, that is not a valid value. Please guess again.")
+            if number < rand_range_low:
+                print(f"Sorry, that number is not within the range of {rand_range_low} and {rand_range_high}. Please guess again.")
+                continue
+            if number > rand_range_high:
+                print(f"Sorry, that number is not within the range of {rand_range_low} and {rand_range_high}. Please guess again.")
+                continue
+            elif number > rand_number:
+                print(f"Sorry, the correct number is lower than your guess of {number}. Please try again.")
                 continue
             elif number < rand_number:
                 print(f"Sorry, the correct number is higher than your guess of {number}. Please try again.")
-                user_attempts += 1
-                try:
-                    number = int(input("Please select a different number from 1 to 10.  "))
-                except ValueError:
-                    print("Sorry, that is not a valid value. Please guess again.")
                 continue
             elif number == rand_number:
                 print(f"Congratulations! {number} is the correct number!")
